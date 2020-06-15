06db8847c01c8438171922feae5e04332aa9b096
Ghost-1050 <xavierlegrand24@gmail.com>
2020-06-15 10:56:17 +0200

    Faute

--- Historique.py
+++ Historique.py
@@ -24,7 +24,7 @@ def histo_part(participant):
                 for exos, nbre in liste_exos2.items():
                     print(f"{exos} pendant {nbre}s x{resultat[4]} série(s)")
             if resultat[3]==3:
-                print(f"\nLe {resultat[2]} vous avez fait {resultat[4]} série(s) de renforcement musculaire"
+                print(f"\nLe {resultat[2]} vous avez fait {resultat[4]} série(s) de Renforcement musculaire"
                       f" au niveau {resultat[6]}.")
                 print("Récapitulatif des exercies effectués :")
                 liste_exos3 = choix_diff3(resultat[6])
